0cf1450db55b77fc216a4e5deb78c0b4cff5d4a3
ShxdedRaichu <cameronjryan27@gmail.com>
2024-02-12 18:16:11 +0000

    created main file

---
+++ main.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds]});
+const commands = require("./commands.json");
+
+// Register slash commands
+const rest = new Discord.REST({version: '10'}).setToken(process.env.TOKEN);
+try {
+    console.log("refreshing slash commands");
+    await rest.put(Routes.applicationCommands(process.env.ID), {body: commands});
+} catch (error) {
+    console.error(error);
+}
+
+// Command Run Loop
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) { return; }
+
+    // Add commands here
+    if (interaction.commandName == 'ping') { interaction.reply('pong'); }
+})
+
+// Run when ready
+client.on('ready', () => {
+    console.log(`logged in as ${client.user.tag}`);
+})
+
+// Login
+client.login(process.env.TOKEN);
